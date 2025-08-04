97eebd6a509d83bbc59306acf5ed19d599573f05
Sharvil Dhumal <sharvildhumal2023@gmail.com>
2025-08-04 15:16:10 +0530

    home page template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './Components/Home/home'; // <-- Add this import
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home], // <-- Add Home here
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('grafana-autobuilder');
