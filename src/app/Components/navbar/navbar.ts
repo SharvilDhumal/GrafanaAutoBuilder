c2a4eb22b10b77a73f5257bb85475d49db1da8af
Sharvil Dhumal <sharvildhumal2023@gmail.com>
2025-08-11 10:46:46 +0530

    navbar ui changes

--- src/app/Components/navbar/navbar.ts
+++ src/app/Components/navbar/navbar.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
