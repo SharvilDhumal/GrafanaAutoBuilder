45595978e48e894b4fb5ca12d45af4a6d1361441
Sharvil Dhumal <sharvildhumal2023@gmail.com>
2025-08-14 14:19:20 +0530

    added navbar in the documentation page

--- src/app/Components/documentation/documentation.ts
+++ src/app/Components/documentation/documentation.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Navbar } from '../../Components/navbar/navbar';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar],
   templateUrl: './documentation.html',
-  styleUrls: ['./documentation.css']
+  styleUrls: ['./documentation.css'],
 })
-export class Documentation {
-
-}
+export class Documentation {}
